5467b9a2ae4df545a3b0fb032e2996e913915df2
Wargen Guitapp <wargenng@gmail.com>
2024-12-30 11:21:35 -0800

    making sure that if cube doesn't exist then do not run the function

--- src/scripts/cube.js
+++ src/scripts/cube.js
@@ -1,30 +1,32 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
+const canvas = document.querySelector("#cube");
+if (canvas) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
-const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(380, 380);
+    renderer.setClearColor(0xf1f1ef, 1);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#cube"),
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(380, 380);
-renderer.setClearColor(0xf1f1ef, 1);
+    camera.position.setZ(5);
 
-camera.position.setZ(5);
+    const geometry = new THREE.BoxGeometry(3, 3, 3);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        wireframe: true,
+    });
 
-const geometry = new THREE.BoxGeometry(3, 3, 3);
-const material = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    cube.rotation.x += 0.005;
-    cube.rotation.y += 0.005;
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        cube.rotation.x += 0.005;
+        cube.rotation.y += 0.005;
+    }
+
+    animate();
 }
-animate();
